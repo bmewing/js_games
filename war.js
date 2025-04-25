871682f25795978a0ebd5fe09b333438bf55d46c
Benjamin Ewing II <bmewing2@outlook.com>
2025-04-25 17:17:10 -0400

    Added Displaying on a Card

--- war.js
+++ war.js
@@ -1,3 +1,15 @@
+const winCounterEl = document.getElementById('win-counter');
+const pDeckEl = document.getElementById('player-deck');
+const oDeckEl = document.getElementById('opponent-deck');
+const pCardEl = document.getElementById('player-card');
+const oCardEl = document.getElementById('opponent-card');
+const pWarEl = document.getElementById('player-war');
+const oWarEl = document.getElementById('opponent-war');
+const pSpoilsEl = document.getElementById('player-spoils');
+const oSpoilsEl = document.getElementById('opponent-spoils');
+const msgEl = document.getElementById('message');
+const controlsEl = document.getElementById('controls');
+
 const suits = ['♥','♦','♣','♠'];
 const ranks = [
     { name: '2', value: 2 },{ name: '3', value: 3 },{ name: '4', value: 4 },{ name: '5', value: 5 },
@@ -9,3 +21,19 @@ function createDeck() {
     const deck = [];
     suits.forEach(suit => ranks.forEach(r => deck.push({ suit, rank: r.name, value: r.value }))); return deck;
 }
+
+function playTurn() {
+    let pCard = pDeck.shift();
+    let oCard = oDeck.shift();
+    displayCard(pCardEl, pCard);
+    displayCard(oCardEl, oCard);
+}
+
+function displayCard(el, card) {
+    el.textContent = card?`${card.rank}${card.suit}`:'';
+}
+
+let pDeck = createDeck();
+let oDeck = createDeck();
+
+pDeckEl.addEventListener('click', playTurn);
